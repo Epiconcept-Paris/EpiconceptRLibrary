64ef461766ac1e7d50f303371b4e626446543c5c
jean-pierre <jean-pierre@5e85e0ee-33d0-4d0d-ad0e-fc9251a6a1c5>
2015-04-29 22:16:37 +0000

    Update ec.xtable

--- trunk/Epiconcepts/R/ec.xtable.R
+++ trunk/Epiconcepts/R/ec.xtable.R
@@ -1,9 +1,10 @@
-ec.xtable <- function(df, align=NULL, digits=2, caption=NA) {
+ec.xtable <- function(df, align=NULL, digits=2, caption="") {
   if (exists("OUTPUT_FORMAT")) {
     require(knitr);
-#     output <- xtable(df, align=align, digits=digits, caption=caption);
-#     print(output, type = "html", include.rownames = F, comment = F);
-     print(kable(df, align=align, digits=digits, caption=caption));
+    require(xtable);
+    output <- xtable(df, align=align, digits=digits, caption=caption);
+    print(output, type = "html", include.rownames = F, comment = F);
+#     print(kable(df, align=align, digits=digits, caption=caption));
 } else {
     print(df);
   }
